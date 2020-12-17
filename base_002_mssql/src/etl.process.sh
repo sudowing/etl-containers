720da793c961b86e452f67c648416a8470b08eb5
Joe Wingard <open-source@joewingard.com>
2020-12-17 13:47:45 -0500

    minor updates (#3)

--- base_002_mssql/src/etl.process.sh
+++ base_002_mssql/src/etl.process.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 . ../log.sh;
 
-sqlcmd="/opt/mssql-tools/bin/sqlcmd"
-
 log info "etl_base_002_mssql";
-log info "sqlcmd: $($sqlcmd -?)";
+log info "SQLCMD: $($SQLCMD -?)";
--- base_002_mssql/src/sample.sh
+++ base_002_mssql/src/sample.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 . ../log.sh;
 
-sqlcmd="/opt/mssql-tools/bin/sqlcmd"
-
 # mssql examples
 log info 'EXPORT [START]';
-$sqlcmd -S $DB_HOST -d $DB_NAME -U $DB_USER -P $DB_PASSWORD -i sql/01_export.sql -o output/some_file.csv;
+$SQLCMD -S $DB_HOST -d $DB_NAME -U $DB_USER -P $DB_PASSWORD -i sql/01_export.sql -o output/some_file.csv;
 log info 'EXPORT [COMPLETE]';
 
 log info 'LOAD [START]';
-$sqlcmd -S $DB_HOST -d $DB_NAME -U $DB_USER -P $DB_PASSWORD -i sql/02_load.sql;
+$SQLCMD -S $DB_HOST -d $DB_NAME -U $DB_USER -P $DB_PASSWORD -i sql/02_load.sql;
 log info 'LOAD [COMPLETE]';
 
